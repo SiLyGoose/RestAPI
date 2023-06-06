84ae6e5d19938ce2eee1ac4470e2193bec57626d
Simon <zzisandstormzz9@gmail.com>
2023-06-06 15:38:58 -0700

    Fix: CORS set to wrong domain

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 // Enable CORS middleware
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+	res.setHeader('Access-Control-Allow-Origin', 'http://javking-api.herokuapp.com');
   	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
   	next();
