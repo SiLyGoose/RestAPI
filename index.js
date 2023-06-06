b63ddd31f44ce425cd65c748fd41e98f9dc1771d
Simon <zzisandstormzz9@gmail.com>
2023-06-06 14:32:54 -0700

    Fix: change content-type

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
   	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
   	next();
 })
 
