c1b37e76fec55e9d248f6825cb9541ed4a775e9a
Simon <zzisandstormzz9@gmail.com>
2023-06-06 17:08:54 -0700

    Fix: attempt x at CORS

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json()); // turns all requests into json
 
 // Enable CORS middleware
 // app.use((req, res, next) => {
-// 	res.setHeader('Access-Control-Allow-Origin', 'http://simonly.herokuapp.com:3000');
+// 	res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
 //   	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 //   	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
 //   	next();
@@ -26,8 +26,14 @@ app.use(express.json()); // turns all requests into json
 // 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
 // 	allowedHeaders: 'Content-Type, application/json',
 //   };
+
+app.use(cors({
+	origin: 'http://127.0.0.1:5500',
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	allowedHeaders: ['Content-Type', 'Authorization']
+  }));
   
-app.use(cors());
+// app.use(cors());
 
 // const cookieList = [];
 const guildMemberList = [];
