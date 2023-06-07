2311a145faba5560d195e68309c2f9bc7029dfbb
Simon <zzisandstormzz9@gmail.com>
2023-06-06 17:47:06 -0700

    Attempt Fix: final CORS.

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(express.json()); // turns all requests into json
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
   	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
+  	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   	next();
-})
+});
   
 // app.use(cors());
 
